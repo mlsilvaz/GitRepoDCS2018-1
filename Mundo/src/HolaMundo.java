c18e4d482d055256b780a0463b40ac1a46bc2680
mlsilvaz <mlsilvaz@gmail.com>
2018-07-07 23:06:42 -0500

    prueba

--- Mundo/src/HolaMundo.java
+++ Mundo/src/HolaMundo.java
@@ -1,7 +1,8 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// Prueba
+		// Nueva Prueba 2
+		System.out.println("Hola Todo el Mundo-Prueba de sincronizacion");
 		System.out.println("Hola Todo el Mundo-Prueba de sincronizacion");
 		System.out.println("Hola Todo el Mundo-Prueba de sincronizacion");
 	}
