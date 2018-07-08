8b36f54be1eea8f04d05a4a4ce76516153ad39bb
mlsilvaz <mlsilvaz@gmail.com>
2018-07-08 01:39:43 -0500

    Nueva Prueba 3

--- Mundo/src/HolaMundo.java
+++ Mundo/src/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 		// Nueva Prueba 2
 		System.out.println("Hola Todo el Mundo-Prueba de sincronizacion");
 		System.out.println("Hola Todo el Mundo-Prueba de sincronizacion");
-		System.out.println("Hola Todo el Mundo-Prueba de sincronizacion");
+	
 	}
 
 }
